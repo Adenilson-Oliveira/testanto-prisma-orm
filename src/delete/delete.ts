d2c8e22bbf0e89b5240ce675904210e19ae107e6
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2022-11-07 09:14:44 -0300

    deletando dados

---
+++ src/delete/delete.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+async function main() {
+  const result = await prisma.matriculas.delete({
+    where: {
+      id: 'a4826a64-6139-4248-b99f-2d0c7c008fb8'
+    }
+  })
+
+  console.log(result);
+
+}
+
+main()
